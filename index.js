9baf14ee625155d3c60c797735085f5a744f9593
ibodev1 <ibrahimodev42@gmail.com>
2022-02-20 15:01:47 +0300

    finished

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const ytdl = require("ytdl-core");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use("/static", express.static("./static"));
 
